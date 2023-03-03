a168f58d7be7681890a55aa97f6fe7d1b4702ef6
rauantasbolat <rauan98ktl@gmail.com>
2023-03-03 13:05:42 +0600

    Initial commit

---
+++ src/main/java/rauan/tasbolat/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,13 @@
+package rauan.tasbolat.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+
+}
